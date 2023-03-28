147ed6b77eb150a3e15d0d70b33ea6ccd45dca31
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-28 23:45:10 +0200

    create a home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:e_commerce_app/pages/login.dart';
+//import 'package:e_commerce_app/pages/login.dart';
 import 'package:e_commerce_app/pages/register.dart';
 import 'package:flutter/material.dart';
 void main() {
