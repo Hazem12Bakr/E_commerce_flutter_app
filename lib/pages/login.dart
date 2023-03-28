67053aff0175a7d048ef367aff7fe113189a48c4
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-28 23:02:28 +0200

    create login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,63 +9,66 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(33.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 65,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                obscureText: false,
-                decoration: decorationtextfield.copyWith(
-                  hintText: "Enter your E-mail : ",
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 65,
                 ),
-              ),
-              const SizedBox(
-                height: 33,
-              ),
-              TextField(
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: decorationtextfield.copyWith(
-                  hintText: "Enter your Password : ",
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: decorationtextfield.copyWith(
+                    hintText: "Enter your E-mail : ",
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 33,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(BTNgreen),
-                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8))),
+                const SizedBox(
+                  height: 33,
                 ),
-                child: Text(
-                  "Sign in",
-                  style: TextStyle(fontSize: 19),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: decorationtextfield.copyWith(
+                    hintText: "Enter your Password : ",
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 33,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("dont have an account? ",style: TextStyle(fontSize: 20)),
-                  TextButton(
-                    onPressed: () {},
-                    child:
-                        Text('Sign up', style: TextStyle(fontSize: 20))),
-                ],
-              )
-            ],
+                const SizedBox(
+                  height: 33,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(BTNgreen),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                  ),
+                  child: Text(
+                    "Sign in",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("dont have an account? ",style: TextStyle(fontSize: 18)),
+                    TextButton(
+                      onPressed: () {},
+                      child:
+                          Text('Sign up', style: TextStyle(fontSize: 18))),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
