d06cf528e7b5cce0c3db270667f0c34855a4a9c1
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 00:04:29 +0200

    put an icon in the action in appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,6 @@
 import 'package:e_commerce_app/shared/colors.dart';
 import 'package:flutter/material.dart';
 
-
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
@@ -12,8 +10,22 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // ignore: prefer_const_literals_to_create_immutables
+        actions: [
+          Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Text(
+                "\$ 13",
+                style: TextStyle(fontSize: 16),
+              ),
+              IconButton(onPressed: (){},
+               icon: Icon(Icons.add_shopping_cart))
+            ],
+          ),
+        ],
         backgroundColor: appbarGreen,
-        title: Text("hazem"),
+        title: Text("Home"),
       ),
     );
   }
