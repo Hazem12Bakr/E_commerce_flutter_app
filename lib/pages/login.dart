82709684e6dc9da597a9c3a6004149f6062ab8b7
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-28 18:07:08 +0200

    created a login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("data")
+          ],
+        ),
+      ),
+    );
+  }
+}
