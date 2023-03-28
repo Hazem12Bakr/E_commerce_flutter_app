0cc212700f2304f31df19110dcb908ef69b1ffd5
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-28 23:50:42 +0200

    created the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-//import 'package:e_commerce_app/pages/login.dart';
+// ignore_for_file: unused_import
+
+import 'package:e_commerce_app/pages/login.dart';
+import 'package:e_commerce_app/pages/home.dart';
 import 'package:e_commerce_app/pages/register.dart';
 import 'package:flutter/material.dart';
 void main() {
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Register(),
+      home:Home(),
     );
   }
 }
