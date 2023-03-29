87af85c10d24710a218590fc1c87d16f4e914710
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 02:17:10 +0200

    creating the action appbar icons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:e_commerce_app/shared/colors.dart';
 import 'package:flutter/material.dart';
@@ -10,17 +10,33 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // ignore: prefer_const_literals_to_create_immutables
         actions: [
           Row(
-            // ignore: prefer_const_literals_to_create_immutables
             children: [
-              Text(
-                "\$ 13",
-                style: TextStyle(fontSize: 16),
+              Stack(
+                children: [
+                  Container(
+                    child: Text(
+                      "8",
+                      style: TextStyle(
+                          fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(211, 164, 255, 193),
+                        shape: BoxShape.circle),
+                  ),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                ],
               ),
-              IconButton(onPressed: (){},
-               icon: Icon(Icons.add_shopping_cart))
+              Padding(
+                padding: const EdgeInsets.only(right: 11.0),
+                child: Text(
+                  "\$ 13",
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
             ],
           ),
         ],
