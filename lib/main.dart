a88125e06b2318007dd712a1cd83d32e0078e51f
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 22:27:24 +0200

    adding the provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, prefer_const_constructors
 
 import 'package:e_commerce_app/pages/details_screen.dart';
 import 'package:e_commerce_app/pages/login.dart';
 import 'package:e_commerce_app/pages/home.dart';
 import 'package:e_commerce_app/pages/register.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 void main() {
   runApp(const MyApp());
 }
