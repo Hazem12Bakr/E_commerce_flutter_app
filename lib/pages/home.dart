65bf874f55518c5d5f2a452c662592349f1eeb61
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 03:32:35 +0200

    create the listtile

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,15 +23,46 @@ class Home extends StatelessWidget {
               currentAccountPicture: CircleAvatar(
                   radius: 55,
                   backgroundImage: AssetImage("assets/img/pic2.jpg")),
-              accountEmail: Text("hazem@yahoo.com"),
+              accountEmail: Text("hazem@yahoo.com",
+              style: TextStyle(
+                color: Color.fromARGB(255, 21, 113, 30)),
+                ),
               accountName: Text("Hazem A.Bakr",
                   style: TextStyle(
-                    color: Color.fromARGB(255, 255, 255, 255),
+                    color: Color.fromARGB(255, 21, 113, 30),
                   )),
             ),
-          ],
+          
+          ListTile(
+   title: Text("Home"),
+   leading: Icon(Icons.home),
+   onTap: () { }
+ ),
+  
+ ListTile(
+   title: Text("My products"),
+   leading: Icon(Icons.add_shopping_cart),
+   onTap: () { }
+ ),
+
+ ListTile(
+   title: Text("About"),
+   leading: Icon(Icons.help_center),
+   onTap: () { }
+ ),
+
+ ListTile(
+   title: Text("Logout"),
+   leading: Icon(Icons.exit_to_app),
+   onTap: () { }
+ ),
+  
+
+ ],
+ ),
+          
         ),
-      ),
+      );
       appBar: AppBar(
         actions: [
           Row(
@@ -69,6 +100,6 @@ class Home extends StatelessWidget {
         backgroundColor: appbarGreen,
         title: Text("Home"),
       ),
-    );
+    
   }
 }
