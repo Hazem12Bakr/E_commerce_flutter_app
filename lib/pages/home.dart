43146dfdeff00ee21449d2b1d46f5182bef934bd
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-30 00:40:42 +0200

    handel the price

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,146 +13,150 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 22),
-        child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 33),
-            itemCount: items.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Details(product: items[index]),
-                    ),
-                  );
-                },
-                child: GridTile(
-                  child: Stack(children: [
-                    Positioned(
-                      top: -3,
-                      bottom: -9,
-                      right: 0,
-                      left: 0,
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(55),
-                          child: Image.asset(items[index].imgpath)),
-                    ),
-                  ]),
-                  footer: GridTileBar(
-// backgroundColor: Color.fromARGB(66, 73, 127, 110),
-                    trailing: Consumer<Cart>(builder: ((context, cartt, child) {
-                      return IconButton(
-                          color: Color.fromARGB(255, 62, 94, 70),
-                          onPressed: () {
-                            cartt.add(items[index]);
-                          },
-                          icon: Icon(Icons.add));
-                    })),
-
-                    leading: Text("\$12.99"),
-
-                    title: Text(""),
-                  ),
-                ),
-              );
-            }),
-      ),
-      drawer: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/img/pic1.jpg"),
-                        fit: BoxFit.cover),
-                  ),
-                  currentAccountPicture: CircleAvatar(
-                      radius: 55,
-                      backgroundImage: AssetImage("assets/img/pic2.jpg")),
-                  accountEmail: Text(
-                    "hazem@yahoo.com",
-                    style: TextStyle(color: Color.fromARGB(255, 21, 113, 30)),
-                  ),
-                  accountName: Text("Hazem A.Bakr",
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 21, 113, 30),
-                      )),
-                ),
-                ListTile(
-                    title: Text("Home"),
-                    leading: Icon(Icons.home),
-                    onTap: () {}),
-                ListTile(
-                    title: Text("My products"),
-                    leading: Icon(Icons.add_shopping_cart),
-                    onTap: () {}),
-                ListTile(
-                    title: Text("About"),
-                    leading: Icon(Icons.help_center),
-                    onTap: () {}),
-                ListTile(
-                    title: Text("Logout"),
-                    leading: Icon(Icons.exit_to_app),
-                    onTap: () {}),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 12),
-              child: Text("Developed by Hazem A.Bakr © 2023",
-                  style: TextStyle(fontSize: 16)),
-            )
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        actions: [
-          Row(
-            children: [
-              Consumer<Cart>(builder: ((context, CartInstancee, child) {
-                return Stack(
-                  children: [
-                    Positioned(
-                      bottom: 24,
-                      child: Container(
-                        child: Text(
-                          "${CartInstancee.selectedProducts.length}",
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(211, 164, 255, 193),
-                            shape: BoxShape.circle),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 22),
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3 / 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 33),
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Details(product: items[index]),
                       ),
+                    );
+                  },
+                  child: GridTile(
+                    child: Stack(children: [
+                      Positioned(
+                        top: -3,
+                        bottom: -9,
+                        right: 0,
+                        left: 0,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(55),
+                            child: Image.asset(items[index].imgpath)),
+                      ),
+                    ]),
+                    footer: GridTileBar(
+// backgroundColor: Color.fromARGB(66, 73, 127, 110),
+                      trailing:
+                          Consumer<Cart>(builder: ((context, cartt, child) {
+                        return IconButton(
+                            color: Color.fromARGB(255, 62, 94, 70),
+                            onPressed: () {
+                              cartt.add(items[index]);
+                            },
+                            icon: Icon(Icons.add));
+                      })),
+
+                      leading: Text("\$12.99"),
+
+                      title: Text(""),
                     ),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-                  ],
+                  ),
                 );
-              })),
-              Padding(
-                padding: const EdgeInsets.only(right: 11.0),
-                child: Text(
-                  "\$ 0",
-                  style: TextStyle(fontSize: 12),
-                ),
+              }),
+        ),
+        drawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/img/pic1.jpg"),
+                          fit: BoxFit.cover),
+                    ),
+                    currentAccountPicture: CircleAvatar(
+                        radius: 55,
+                        backgroundImage: AssetImage("assets/img/pic2.jpg")),
+                    accountEmail: Text(
+                      "hazem@yahoo.com",
+                      style: TextStyle(color: Color.fromARGB(255, 21, 113, 30)),
+                    ),
+                    accountName: Text("Hazem A.Bakr",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 21, 113, 30),
+                        )),
+                  ),
+                  ListTile(
+                      title: Text("Home"),
+                      leading: Icon(Icons.home),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("My products"),
+                      leading: Icon(Icons.add_shopping_cart),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("About"),
+                      leading: Icon(Icons.help_center),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("Logout"),
+                      leading: Icon(Icons.exit_to_app),
+                      onTap: () {}),
+                ],
               ),
+              Container(
+                margin: EdgeInsets.only(bottom: 12),
+                child: Text("Developed by Hazem A.Bakr © 2023",
+                    style: TextStyle(fontSize: 16)),
+              )
             ],
           ),
-        ],
-        backgroundColor: appbarGreen,
-        title: Text("Home"),
-      ),
-    );
+        ),
+        appBar: AppBar(
+          actions: [
+            Consumer<Cart>(
+              builder: ((context, classInstancee, child) {
+                return Row(children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        bottom: 24,
+                        child: Container(
+                          child: Text(
+                            "${classInstancee.selectedProducts.length}",
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.add_shopping_cart)),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 11.0),
+                    child: Text(
+                      "\$ ${classInstancee.price}",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ]);
+              }),
+            )
+          ],
+        ));
+
+    backgroundColor:
+    appbarGreen;
+    title:
+    Text("Home");
   }
 }
