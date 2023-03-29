df9eae50bab855fdaff9f871ba0a0dbce6849f3f
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 20:48:12 +0200

    setting the location to the items

--- lib/model/items.dart
+++ lib/model/items.dart
@@ -7,20 +7,22 @@ import 'package:flutter/material.dart';
 class Item {
   String imgpath;
   double price;
+  String location;
 
   Item({
     required this.imgpath,
     required this.price,
+    this.location = "main branch",
   });
 }
 
 final List<Item> items = [
-    Item(imgpath: "assets/img/car1.jpg", price: 520.99),
-    Item(imgpath: "assets/img/car2.jpg", price: 420.99),
-    Item(imgpath: "assets/img/car3.jpg", price: 420.99),
-    Item(imgpath: "assets/img/car4.jpg", price: 400.99),
-    Item(imgpath: "assets/img/car5.jpg", price: 400.99),
-    Item(imgpath: "assets/img/car6.jpg", price: 500.99),
+    Item(imgpath: "assets/img/car1.jpg", price: 520.99,location: "our branches"),
+    Item(imgpath: "assets/img/car2.jpg", price: 420.99,location: "our branches"),
+    Item(imgpath: "assets/img/car3.jpg", price: 420.99,location: "our branches"),
+    Item(imgpath: "assets/img/car4.jpg", price: 400.99,location: "our branches"),
+    Item(imgpath: "assets/img/car5.jpg", price: 400.99,location: "our branches"),
+    Item(imgpath: "assets/img/car6.jpg", price: 500.99,location: "our branches"),
     Item(imgpath: "assets/img/car7.jpg", price: 780.99),
     Item(imgpath: "assets/img/car8.jpg", price: 600.99),
   ];
--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -133,7 +133,7 @@ class _DetailsState extends State<Details> {
                       width: 3,
                     ),
                     Text(
-                      "Flower Shop",
+                      widget.product.location,
                       style: TextStyle(fontSize: 19),
                     ),
                   ],
