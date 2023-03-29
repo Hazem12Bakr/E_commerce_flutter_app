20d7c374efa4b0a06a85a90e48a287b71065870b
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 02:20:14 +0200

    the appbar created

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,16 +15,19 @@ class Home extends StatelessWidget {
             children: [
               Stack(
                 children: [
-                  Container(
-                    child: Text(
-                      "8",
-                      style: TextStyle(
-                          fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                  Positioned(
+                    bottom: 24,
+                    child: Container(
+                      child: Text(
+                        "8",
+                        style: TextStyle(
+                            fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle),
                     ),
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(211, 164, 255, 193),
-                        shape: BoxShape.circle),
                   ),
                   IconButton(
                       onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
