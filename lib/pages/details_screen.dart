c9de1d10f27a9ebc8dc128252b822a8dc9b3f9bf
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 17:55:49 +0200

    the broduct details

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_label, prefer_const_constructors
+// ignore_for_file: unused_label, prefer_const_constructors, sort_child_properties_last
 
 import 'package:e_commerce_app/shared/colors.dart';
 import 'package:flutter/material.dart';
@@ -22,8 +22,7 @@ class Details extends StatelessWidget {
                       child: Text(
                         "8",
                         style: TextStyle(
-                            fontSize: 16, 
-                            color: Color.fromARGB(255, 0, 0, 0)),
+                            fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
                       ),
                       padding: EdgeInsets.all(5),
                       decoration: BoxDecoration(
@@ -32,8 +31,7 @@ class Details extends StatelessWidget {
                     ),
                   ),
                   IconButton(
-                      onPressed: () {}, 
-                      icon: Icon(Icons.add_shopping_cart)),
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
                 ],
               ),
               Padding(
@@ -49,15 +47,64 @@ class Details extends StatelessWidget {
         backgroundColor: appbarGreen,
         title: Text("Details Screen"),
       ),
-     
-     
       body: Column(
         children: [
           Image.asset("assets/img/car7.jpg"),
-          SizedBox(height: 11,),
-          Text("\$ 400.99", style: TextStyle(fontSize: 20),),
-          SizedBox(height: 16,),
-
+          SizedBox(
+            height: 11,
+          ),
+          Text(
+            "\$ 400.99",
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(4),
+                child: Text(
+                  "New",
+                  style: TextStyle(fontSize: 12),
+                ),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 129, 129),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+              Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Icon(
+                    Icons.star,
+                    size: 23,
+                    color: Color.fromARGB(255, 255, 191, 0),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 23,
+                    color: Color.fromARGB(255, 255, 191, 0),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 23,
+                    color: Color.fromARGB(255, 255, 191, 0),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 23,
+                    color: Color.fromARGB(255, 255, 191, 0),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 23,
+                    color: Color.fromARGB(255, 255, 191, 0),
+                  ),
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
