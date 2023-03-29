3462d55d074973fd8c854089c276a0d5d4a58902
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-29 17:45:24 +0200

    add a details screen

---
+++ lib/pages/details_screen.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: unused_label, prefer_const_constructors
+
+import 'package:e_commerce_app/shared/colors.dart';
+import 'package:flutter/material.dart';
+
+class Details extends StatelessWidget {
+  const Details({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    bottom: 24,
+                    child: Container(
+                      // ignore: sort_child_properties_last
+                      child: Text(
+                        "8",
+                        style: TextStyle(
+                            fontSize: 16, 
+                            color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {}, 
+                      icon: Icon(Icons.add_shopping_cart)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 11.0),
+                child: Text(
+                  "\$ 13",
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+            ],
+          ),
+        ],
+        backgroundColor: appbarGreen,
+        title: Text("Details Screen"),
+      ),
+     
+     
+      body: Column(
+        children: [
+          Image.asset("assets/img/car7.jpg"),
+          SizedBox(height: 11,),
+          Text("\$ 400.99", style: TextStyle(fontSize: 20),),
+          SizedBox(height: 16,),
+
+        ],
+      ),
+    );
+  }
+}
