681f9ed5b3be4de97a393204f019b6662bc5511a
hazembakr <hazem.abobakr.200320@btu.edu.eg>
2023-03-30 02:44:25 +0200

    giving names to the items

--- lib/model/items.dart
+++ lib/model/items.dart
@@ -8,23 +8,25 @@ class Item {
   String imgpath;
   double price;
   String location;
+  String name;
 
   Item({
     required this.imgpath,
     required this.price,
     this.location = "main branch",
+    required this.name ,
   });
 }
 
 final List<Item> items = [
-    Item(imgpath: "assets/img/car1.jpg", price: 520.99,location: "our branches"),
-    Item(imgpath: "assets/img/car2.jpg", price: 420.99,location: "our branches"),
-    Item(imgpath: "assets/img/car3.jpg", price: 420.99,location: "our branches"),
-    Item(imgpath: "assets/img/car4.jpg", price: 400.99,location: "our branches"),
-    Item(imgpath: "assets/img/car5.jpg", price: 400.99,location: "our branches"),
-    Item(imgpath: "assets/img/car6.jpg", price: 500.99,location: "our branches"),
-    Item(imgpath: "assets/img/car7.jpg", price: 780.99),
-    Item(imgpath: "assets/img/car8.jpg", price: 600.99),
+    Item(name:"product1" ,imgpath: "assets/img/car1.jpg", price: 520.99,location: "our branches"),
+    Item(name:"product2" ,imgpath: "assets/img/car2.jpg", price: 420.99,location: "our branches"),
+    Item(name:"product3" ,imgpath: "assets/img/car3.jpg", price: 420.99,location: "our branches"),
+    Item(name:"product4" ,imgpath: "assets/img/car4.jpg", price: 400.99,location: "our branches"),
+    Item(name:"product5" ,imgpath: "assets/img/car5.jpg", price: 400.99,location: "our branches"),
+    Item(name:"product6" ,imgpath: "assets/img/car6.jpg", price: 500.99,location: "our branches"),
+    Item(name:"product7" ,imgpath: "assets/img/car7.jpg", price: 780.99),
+    Item(name:"product8" ,imgpath: "assets/img/car8.jpg", price: 600.99),
   ];
 
 
